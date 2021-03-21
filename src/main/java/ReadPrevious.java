6ee6373578992bd70fdc4b68f8b642ffe04b01a6
SamatarMumin <35471795+SamatarMumin@users.noreply.github.com>
2021-03-20 22:05:33 -0400

    Update ReadPrevious.java

--- src/main/java/ReadPrevious.java
+++ src/main/java/ReadPrevious.java
@@ -1,45 +1,43 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 
 public class ReadPrevious {
-    private ArrayList<String> oldMasterBankAccs;
-    public BufferedReader bufferReader = null;
+    public static  ArrayList<String> oldMasterBankAccs = new ArrayList<String>(); 
+	public ReadPrevious(ArrayList<String> oldMasterBankAccs) {
+		 this.oldMasterBankAccs = oldMasterBankAccs;
+	  }
+	
+	public static void ReadFile() {
+	     BufferedReader bufferReader = null;
+		  try {
+		   String strCurrentLine;
+		   int i = 0;
+		   String array[] = null;
+		   bufferReader = new BufferedReader(new FileReader("mastertbankaccountfile.txt"));
 
-    public ReadPrevious(ArrayList<String> oldMasterBankAccs) {
-        this.oldMasterBankAccs = oldMasterBankAccs;
-    }
-
-    public void ReadFile() {
-        try {
-            String strCurrentLine;
-            bufferReader = new BufferedReader(new FileReader("masterbankaccountfile.txt"));
-
-            while ((strCurrentLine = bufferReader.readLine()) != null) {
-                System.out.println(strCurrentLine);
-            }
-
-        } catch (IOException e) {
-
-            e.printStackTrace(); //add error when completed
-
-        } finally {
-            try {
-                if (bufferReader != null)
-                    bufferReader.close();
-            } catch (IOException ex) {
-                ex.printStackTrace(); //Add Error Function when completed
-            }
-        }
-    }
-
-    static void ParseDoc() {
-    }
-
-    public void main(String[] args) {
-        ReadFile();
-    }
-
+		   while ((strCurrentLine = bufferReader.readLine()) != null) {
+			    ParseDoc(strCurrentLine,array);
+		   }
+		  } catch (IOException e) {
+		   e.printStackTrace(); //add error when completed
+		  } finally {
+		   try {
+		    if (bufferReader != null)
+		    	bufferReader.close();
+		   } catch (IOException ex) {
+		    ex.printStackTrace(); //Add Error Function when completed
+		   }
+		  }
+	  }
+	
+	static void ParseDoc(String strCurrentLine, String[] array) {
+	    array = strCurrentLine.split("\\s+");
+	    System.out.println(Arrays.toString(array));	
+	  }
 }
+
